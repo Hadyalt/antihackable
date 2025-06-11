cf1d6197eb1f0f5b0cb2fbe90162c1d293462c3b
Jasper85125 <Jaspervanderlaan1105@gmail.com>
2025-06-11 14:59:46 +0200

    Remove username verification from reset_password method in ServiceEngineer

--- serviceEngineer/ServiceEngineer.py
+++ serviceEngineer/ServiceEngineer.py
@@ -1,14 +1,10 @@
 from DbContext.DbContext import DbContext
-from Login.verification import Verification
 
 class ServiceEngineer:
     def __init__(self):
         self.db_context = DbContext()
 
     def reset_password(self, username, new_password):
-        if not Verification.verify_username(username):
-            print("User verification failed.")
-            return
 
         connection = self.db_context.connect()
         if connection:
